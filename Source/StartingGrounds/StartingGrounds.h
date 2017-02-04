8b6efc64006b5f0d76d0f0a8529bacd9ad3bc803
OWNER-PC\Owner <tim.k96@hotmail.com>
2017-02-04 14:47:40 +1100

    Enabled Git LFS and added FPS content

---
+++ Source/StartingGrounds/StartingGrounds.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __STARTINGGROUNDS_H__
+#define __STARTINGGROUNDS_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/StartingGrounds/StartingGroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "StartingGrounds.h"
+#include "StartingGroundsGameMode.h"
+#include "StartingGroundsHUD.h"
+#include "StartingGroundsCharacter.h"
+
+AStartingGroundsGameMode::AStartingGroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AStartingGroundsHUD::StaticClass();
+}
